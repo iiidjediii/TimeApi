103a0804408de2f3b24d1d967e3341d1100ab745
iiidjediii <iiidjediii@mail.ru>
2020-12-09 21:45:19 +0300

    comment:add comments

--- core/utils.py
+++ core/utils.py
@@ -6,14 +6,17 @@ DIFFERENCE = 50
 
 
 def check_time(value):
+    # check that the time value is in the correct format
     try:
         value = int(value)
     except:
         return False
+    # checking for server response existence
     try:
         response = requests.get(TIME_URL)
     except:
         raise APIException('without connection internet')
+    #
     if response.status_code == 200:
         time = response.json().get('unixtime')
         valid = abs(int(time) - int(value)) < DIFFERENCE

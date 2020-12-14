91d528c3bdd9749db3bfdcf7c9df5df09cbb2276
iiidjediii <iiidjediii@mail.ru>
2020-12-14 20:55:45 +0300

    change user_exist + jsonResponse

--- core/middleware.py
+++ core/middleware.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseForbidden, HttpResponse
+from django.http import HttpResponseForbidden, JsonResponse
 
 from .services import is_token_valid
 
@@ -6,12 +6,9 @@ from .services import is_token_valid
 def token_check(get_response):
     def middleware(request):
         if not is_token_valid(request.headers.get('Authorization')):
-            return HttpResponseForbidden("Authorization error!")
-
+            return JsonResponse({"detail": "Authorization error!"}, status=400)
         response = get_response(request)
-
         return response
-
     return middleware
 
 
@@ -27,8 +24,5 @@ class CustomExcMiddleware:
         return response
 
     def process_exception(self, request, exception):
-        try:
-            if isinstance(exception, Exception):
-                return HttpResponse(exception.__module__, status=500)
-        except:
-            return HttpResponse('something unexpected')
+        if isinstance(exception, Exception):
+            return JsonResponse({"Error": exception.__module__}, status=500)
--- core/services.py
+++ core/services.py
@@ -3,8 +3,9 @@ import base64
 from .models import User
 
 
-def get_user(username, password):
-    return User.objects.get(user_name=username, password=password)
+def user_exist(username, password):
+    return User.objects.filter(user_name=username, password=password).exists()
+    # return User.objects.get(user_name=username, password=password)
 
 
 def is_token_valid(token):
@@ -17,7 +18,7 @@ def is_token_valid(token):
         bytes_token = base64.b64decode(base64_token)
         username, password = bytes_token.decode('ascii').split(':')
 
-        if get_user(username, password):
+        if user_exist(username, password):
             result = True
     except BaseException as exc:
         print(exc)
